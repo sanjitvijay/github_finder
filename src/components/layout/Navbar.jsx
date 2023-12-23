b2ad97a62e2721bb48f4008a654cae726117c42b
Sanjit Vijay <sanjitv@umich.edu>
2023-12-23 14:40:50 -0500

    Fixed logo home button clear

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -12,7 +12,10 @@ function Navbar({title}) {
       <div className='container mx-auto'>
         <div className='flex-none px-2 mx-2'>
             <FaGithub className='inline pr-2 text-3xl'/>
-            <Link to='/' classname='text-lg font-bold align-middle'>
+            <Link 
+              onClick={()=>dispatch({type: "CLEAR_USERS"})}
+              to='/' 
+              classname='text-lg font-bold align-middle'>
               {title}
             </Link>
         </div>
